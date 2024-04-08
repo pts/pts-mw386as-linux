772a006d69bb22f0d9a5041f6dbee283bb32a3fd
Peter Szabo <pts@fazekas.hu>
2024-04-08 02:03:41 +0200

    added extraction of ddk32.dd and as to compile.sh

--- compile.sh
+++ compile.sh
@@ -21,6 +21,47 @@ shift
 
 set -ex
 
+# From Coherent 4.2.10, -r-x--x--x 1 pts pts 69024 Nov 11  1992 as
+if ! test -f as; then
+  if ! test -f ddk32.dd; then
+    if test -f "Coherent Unix 4.2.10 (1994) (3.5-1.44mb)/base/ddk32.dd"; then
+      tools/mv "Coherent Unix 4.2.10 (1994) (3.5-1.44mb)/base/ddk32.dd" ./
+    else
+      if ! test -f "Coherent Unix 4.2.10 (1994) (3.5-1.44mb).7z"; then
+        : 'Download "Coherent Unix 4.2.10 (1994) (3.5-1.44mb).7z" from https://winworldpc.com/download/532ebb91-483c-11ed-b2b1-0200008a0da4'
+        exit 1
+      fi
+      7z x -y "Coherent Unix 4.2.10 (1994) (3.5-1.44mb).7z" "Coherent Unix 4.2.10 (1994) (3.5-1.44mb)/base/ddk32.dd" ||:
+      if ! test -f "Coherent Unix 4.2.10 (1994) (3.5-1.44mb)/base/ddk32.dd"; then
+        tools/tiny7zx x -y "Coherent Unix 4.2.10 (1994) (3.5-1.44mb).7z" ||:
+      fi
+      if ! test -f "Coherent Unix 4.2.10 (1994) (3.5-1.44mb)/base/ddk32.dd"; then
+        : "Extraction failed."
+        exit 1
+      fi
+      tools/mv "Coherent Unix 4.2.10 (1994) (3.5-1.44mb)/base/ddk32.dd" ./
+      tools/rm -rf "Coherent Unix 4.2.10 (1994) (3.5-1.44mb)"
+    fi
+  fi
+  test -f ddk32.dd
+  # Extract single file as form disk image ddk32.dd.
+  tools/miniperl-5.004.04 -e '
+      BEGIN { $^W = 1 } use integer; use strict;
+      $_ = "000102030405060708090b0c0d0e0f101112131415161718191a1b1c1d
+      1e1f202122232425262728292a2b2c2d2e2f303132333435363738393a3b3c3d
+      3e3f404142434445464748494a4b4c4d4e4f505152535455565758595a5b5c5d
+      5e5f606162636465666768696a6b6c6d6e6f707172737475767778797a7b7c7d
+      7e7f8081828384858687"; s@\s+@@g; $_ = pack("H*", $_); my $s;
+      my $size = 69024; my $try; binmode(STDIN); binmode(STDOUT);
+      s@(.)@
+        die 3 if !seek(STDIN, (ord($1) + 1123) << 9, 0);
+        $try = ($size > 0x200 ? 0x200 : $size);
+        die 4 if read(STDIN, $s, $try) != $try;
+        die 5 if !print($s);
+        $size -= length($s);
+      @gse; die$@if$@' <ddk32.dd >as
+fi
+
 old_path="$PATH"
 export PATH=/dev/null/nopath
 
