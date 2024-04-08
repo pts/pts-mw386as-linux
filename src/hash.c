4de488d4e9ff319d8f289bb5b00f01d6988f7f5a
Peter Szabo <pts@fazekas.hu>
2024-04-08 17:52:11 +0200

    fixed compilation errors and warnings in hash.c

--- src/hash.c
+++ src/hash.c
@@ -12,21 +12,21 @@ register char *p;
 	 * Tables for the table-driven CRC16 algorithm.
 	 * This should be relatively uniform statistically.
 	 */
-	static	unsigned short crctab1[] = {
+	static	const unsigned short crctab1[] = {
 		0000000,	0140301,	0140601,	0000500,
 		0141401,	0001700,	0001200,	0141101,
 		0143001,	0003300,	0003600,	0143501,
 		0002400,	0142701,	0142201,	0002100
 	};
 
-	static	unsigned short crctab2[] = {
+	static	const unsigned short crctab2[] = {
 		0000000,	0146001,	0154001,	0012000,
 		0170001,	0036000,	0024000,	0162001,
 		0120001,	0066000,	0074000,	0132001,
 		0050000,	0116001,	0104001,	0043000
 	};
 
-	for(h = 0; c = *p; p++) {
+	for(h = 0; (c = *p); p++) {
 		tmp = c ^ h;
 		h = (h >> 8) ^ crctab1[tmp & 15] ^ crctab2[tmp >> 4];
 	}
